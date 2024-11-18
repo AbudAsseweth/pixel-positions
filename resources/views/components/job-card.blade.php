0841dfcca0496b25fe85709144bdb378b0d6868a
Muhammad Yazid <its.yazid.abud@gmail.com>
2024-11-18 21:49:21 +0700

    Episode 27 final code

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="flex flex-col rounded-xl bg-white/5 p-4">
+    <div class="text-sm">Laracasts</div>
+    <div class="py-8 text-center font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div class="mt-auto flex items-center justify-between">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="https://placehold.co/42x42" alt="" class="rounded-xl">
+    </div>
+</div>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,2 @@
+<a href=""
+    class="rounded-xl bg-white/10 px-2 py-1 text-xs transition-colors duration-300 hover:bg-white/20">{{ $slot }}</a>
